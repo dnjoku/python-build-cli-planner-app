68d68645da639f36882bf0ee886272712029d8e2
Dan Njoku <dnjoku@ups.com>
2021-12-09 18:05:46 -0500

    deadlined reminder class construction fixes

--- src/reminder.py
+++ src/reminder.py
@@ -7,7 +7,7 @@ class PrefixedReminder:
         self.text = prefix + '<placeholder_text>'
 
 class PoliteReminder(PrefixedReminder):
-    def __init__(self, text):
+    def __init__(self, text, date=None):
         super().__init__(prefix="Please remember to ")
         self.text = self.prefix + text
 
